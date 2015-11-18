3eb6672f59b77c8e025af40741b975749fffdd7e
Vlad Lifliand <vlif@google.com>
2015-11-17 22:31:42 -0800

    Code with no debugger enabled

---
+++ randomgenerator.py
@@ -0,0 +1,14 @@
+import random
+from flask import request
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/random')
+def getrandom():
+    n = int(request.args.get('n', 10000))
+    x = random.randint(0, n)
+    return 'The random number in the range of [0..%d] is %d' % (n, x)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
